076b61000f813908464e41ab4f0fc2d8226e5195
dani heredia <daniel.heredia@manomano.com>
2024-06-04 23:13:48 +0200

    Fetching data from 2 APIs

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+//const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat/says/${}?fontSize=50&fontColor=red'
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+
+export function App () {
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        const firstWord = fact.split(' ')[0]
+        // const threeFirstWords = fact.split(' ', 3).join(' ')
+
+        console.log(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+
+        fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+          .then(res => res.json())
+          .then(response => {
+            console.log(response)
+          })
+      })
+  }, [])
+
+  return (
+    <main>
+      <h1> App de gatitos </h1>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first word for ${fact}`} />}
+    </main>
+  )
+}
