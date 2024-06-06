a81d9f64c7ef6a49da35d2728dd5e90ffd8ec43a
dani heredia <daniel.heredia@manomano.com>
2024-06-06 16:50:43 +0200

    First playwright tests

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ export function App () {
       <button onClick={handleClick}> Get new Fact </button>
       {fact && <p>{fact}</p>}
       {imageUrl && <img src={`${imageUrl}`} alt={`Image extracted using the first word for ${fact}`} />}
-      <Otro />
+      { /* <Otro /> */}
     </main>
   )
 }
